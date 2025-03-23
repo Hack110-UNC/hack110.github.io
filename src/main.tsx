40a60dd7bd2df3cd9eeae23a9bb990a282572b7d
root <root@ConorsComputer>
2025-03-23 16:03:31 -0400

    hello

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css"; 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import App from './pages/hack110';
 import About from './pages/about';
 import Map from './pages/map';
 import Workshops from "./pages/workshops";
 
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <Routes>
+        <Route path="/hack110/" element={<Navigate to="/hack110" replace />} />
         <Route path="/hack110" element={<App />} />
         <Route path="/about" element={<About />} />
         <Route path="/map" element={<Map />} />
