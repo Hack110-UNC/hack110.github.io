a44d541c6d751a95166ef82b825d80815c946593
root <root@ConorsComputer>
2025-03-23 15:34:09 -0400

    sdfasd

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'; 
 import tailwindcss from "tailwindcss";
 
 export default defineConfig({
+  plugins: [
+    react(), // Add React plugin first
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss()],
