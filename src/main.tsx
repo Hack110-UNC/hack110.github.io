d82a64074434ea324b1b0b23fd0aacb80c371ff6
root <root@ConorsComputer>
2025-03-23 15:59:16 -0400

    hello

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css"; 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import App from './pages/landing';
+import App from './pages/hack110';
 import About from './pages/about';
 import Map from './pages/map';
 import Workshops from "./pages/workshops";
